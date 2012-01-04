268eaeb8256ff6bd03b49888f76ab8f0c221a58c
Eric Montgomery <ericwmontgomery@gmail.com>
2012-01-04 16:17:15 -0500

    create spring config classes

---
+++ src/main/java/org/carillonlib/webcore/application/config/CarillonMvcConfig.java
@@ -0,0 +1,14 @@
+package org.carillonlib.webcore.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class CarillonMvcConfig {
+	@Bean
+	public InternalResourceViewResolver configureInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
