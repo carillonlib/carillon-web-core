68ea5d619224c27697de57caf686ccc8a02c8563
Eric Montgomery <ericwmontgomery@gmail.com>
2012-01-20 09:34:25 -0500

    log startup info

--- src/main/java/org/carillonlib/webcore/application/config/CarillonWebApplicationInitializer.java
+++ src/main/java/org/carillonlib/webcore/application/config/CarillonWebApplicationInitializer.java
@@ -4,14 +4,18 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class CarillonWebApplicationInitializer implements WebApplicationInitializer {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
+		log.info(getClass().getSimpleName() + " beginning initialization for context: " + servletContext.getContextPath());
 		AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
 		mvcContext.register(CarillonMvcConfig.class);
 		mvcContext.scan("org.carillonlib");
