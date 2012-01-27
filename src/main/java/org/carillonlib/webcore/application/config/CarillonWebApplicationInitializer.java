d35424ade826e0c18381e742095b081985edd2d6
Eric Montgomery <ericwmontgomery@gmail.com>
2012-01-27 10:57:51 -0500

    add spring security filter

--- src/main/java/org/carillonlib/webcore/application/config/CarillonWebApplicationInitializer.java
+++ src/main/java/org/carillonlib/webcore/application/config/CarillonWebApplicationInitializer.java
@@ -1,5 +1,9 @@
 package org.carillonlib.webcore.application.config;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -8,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class CarillonWebApplicationInitializer implements WebApplicationInitializer {
@@ -15,7 +20,9 @@ public class CarillonWebApplicationInitializer implements WebApplicationInitiali
 	private Logger log = LoggerFactory.getLogger(getClass());
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
+		servletContext.setInitParameter("contextConfigLocation", "/WEB-INF/security-app-context.xml");
 		log.info(getClass().getSimpleName() + " beginning initialization for context: " + servletContext.getContextPath());
+
 		AnnotationConfigWebApplicationContext mvcContext = new AnnotationConfigWebApplicationContext();
 		log.debug("Setting active spring profile: " + getActiveProfile());
 		mvcContext.getEnvironment().setActiveProfiles(getActiveProfile());
@@ -26,6 +33,11 @@ public class CarillonWebApplicationInitializer implements WebApplicationInitiali
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(mvcContext));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping("/app/*");
+
+		// set up spring security
+		DelegatingFilterProxy delegatingFilterProxy = new DelegatingFilterProxy("springSecurityFilterChain", mvcContext);
+		FilterRegistration fr = servletContext.addFilter("springSecurityFilterChain", delegatingFilterProxy);
+		fr.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), true, "/*");
 	}
 
 	protected String getActiveProfile() {
